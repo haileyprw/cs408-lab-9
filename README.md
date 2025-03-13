cbc34360b33182acffb4c4e988221412bbf7cce6
Hailey Revel-Whitaker <haileyrevelwhita@u.boisestate.edu>
2025-03-12 20:51:55 -0600

    lab completed!

--- README.md
+++ README.md
@@ -2,23 +2,33 @@
 
 ## Overview
 
-TODO: Write a project description
+This project showcased provided HTML and CSS with added JavaScript. It creates a game in browser in which a user can use teh A, W, S, and D keys to control an evil ball to capture the other moving balls.
+
+## Getting Started
+
+To get started, clone this repository and run the following commands:
+
+```bash
+npm install
+```
+This will install the necessary dependencies for the project.
+
+## Development
+
+It is recommended to use the VSCode Live Server extension to run the project
+locally. This will allow you to see changes in real-time as you make them. There
+is no need to run a build process or refresh the page manually. Additionally,
+you do not need to setup a local server to run the project.
+
+## Testing
+
+To run the tests for the project, run the following command:
+
+```bash
+npm test
+```
 
 ## Sources and Credits
 
-TODO: You must credit the sources and authors of any code, libraries, or other
-assets you use in your project. If you leave this section blank, your project
-will be considered in violation of the Academic Honesty policy unless you truly
-created everything from scratch with no outside help. If you need to use a
-source that you cannot credit (e.g. a classmate's work), you must get explicit
-permission from your instructor.
-
-A simple bulleted list below is sufficient. For example:
-
-- Bootstrap: https://getbootstrap.com/
-- jQuery: https://jquery.com/
-- Background image: https://unsplash.com/photos/...
-- Sound effects: https://freesound.org/people/...
-- Icons: https://fontawesome.com/
-- Fonts: https://fonts.google.com/
-- etc.
+- Developer.mozilla Classes in JavaScript (https://developer.mozilla.org/en-US/docs/Learn_web_development/Extensions/Advanced_JavaScript_objects/Classes_in_JavaScript)
+- Developer.mozilla JavaScript Objects (https://developer.mozilla.org/en-US/docs/Learn_web_development/Core/Scripting/Object_basics)
--- js/main.js
+++ js/main.js
@@ -18,14 +18,107 @@ function randomRGB() {
   return `rgb(${random(0, 255)},${random(0, 255)},${random(0, 255)})`;
 }
 
-class Ball {
-  constructor(x, y, velX, velY, color, size) {
+// Create a reference to the paragraph element that displays the ball count
+const ballCountParagraph = document.getElementById('ballCount');
+
+// Variable to keep track of the number of balls
+let ballCount = 0;
+let winPossible = false;
+
+// Function to update the ball count display
+function updateBallCount() {
+  ballCountParagraph.textContent = `Balls on screen: ${ballCount}`;
+}
+
+class Shape {
+  constructor(x, y, velX, velY) {
     this.x = x;
     this.y = y;
     this.velX = velX;
     this.velY = velY;
+  }
+}
+
+class EvilCircle extends Shape {
+  constructor(x, y) {
+    // Call the shape constructor
+    super(x, y, 20, 20);
+
+    // Initialize color and size
+    this.color = "white";
+    this.size = 10;
+
+    // Enables user control
+    window.addEventListener("keydown", (e) => {
+      switch (e.key) {
+        case "a":
+          this.x -= this.velX;
+          break;
+        case "d":
+          this.x += this.velX;
+          break;
+        case "w":
+          this.y -= this.velY;
+          break;
+        case "s":
+          this.y += this.velY;
+          break;
+      }
+    });
+  }
+
+  draw() {
+    ctx.beginPath();
+    ctx.strokeStyle = this.color;
+    ctx.lineWidth = 3;
+    ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
+    ctx.stroke();
+  }
+
+  checkBounds() {
+    if (this.x + this.size >= width) {
+      this.x = this.x - this.size;
+    }
+
+    if (this.x - this.size <= 0) {
+      this.x = this.x + this.size;
+    }
+
+    if (this.y + this.size >= height) {
+      this.y = this.y - this.size;
+    }
+
+    if (this.y - this.size <= 0) {
+      this.y = this.y + this.size;
+    }
+  }
+
+  collisionDetect() {
+    for (const ball of balls) {
+      if (ball.exists) {
+        const dx = this.x - ball.x;
+        const dy = this.y - ball.y;
+        const distance = Math.sqrt(dx * dx + dy * dy);
+
+        if (distance < this.size + ball.size) {
+          ball.exists = false;
+          ballCount--;
+          updateBallCount();
+        }
+      }
+    }
+  }
+}
+
+class Ball extends Shape {
+  constructor(x, y, velX, velY, color, size) {
+    // Call the shape constructor
+    super(x, y, velX, velY);
+
+    // Initialize color and size
     this.color = color;
     this.size = size;
+    this.exists = true;
   }
 
   draw() {
@@ -58,7 +151,7 @@ class Ball {
 
   collisionDetect() {
     for (const ball of balls) {
-      if (!(this === ball)) {
+      if (ball.exists && this !== ball) {
         const dx = this.x - ball.x;
         const dy = this.y - ball.y;
         const distance = Math.sqrt(dx * dx + dy * dy);
@@ -87,19 +180,33 @@ while (balls.length < 25) {
   );
 
   balls.push(ball);
+  ballCount++;
+  updateBallCount();
+  winPossible = true;
 }
+
+const evil = new EvilCircle(width/2, height/2); 
 
 function loop() {
   ctx.fillStyle = "rgba(0, 0, 0, 0.25)";
   ctx.fillRect(0, 0, width, height);
 
   for (const ball of balls) {
-    ball.draw();
-    ball.update();
-    ball.collisionDetect();
+    if (ball.exists) {
+      ball.draw();
+      ball.update();
+      ball.collisionDetect();
+    }
   }
+  evil.draw();
+  evil.checkBounds();
+  evil.collisionDetect();
 
   requestAnimationFrame(loop);
+  
+  if (winPossible == true && ballCount == 0) {
+    
+  }
 }
 
 loop();
